dd1ad664127eff4581e4e2efc52fe58f03d80cec
GAVIN SIMPSON <gdsimpson@gmail.com>
2022-11-22 13:55:36 +0100

    goodbye

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Goodbye, this website will not be availible on the 28th of november
+        2022, this was created on the 22nd of november 2022
+      </h1>
+      <p>
+        Nur der Tod währt alle Zeit
+        <br />
+        Er flüstert unterm Tannenzweig
+        <br />
+        Muss alles in sein Dunkel ziehen
+        <br />
+        Sogar die Sonne wird verglühen
+        <br />
+        Doch keine Angst, wir sind bei dir
+        <br />
+        Ein letztes Mal, so singen wir
+        <br />
+        Adieu, Goodbye, Auf Wiedersehen
+        <br />
+        Den letzten Weg musst du alleine gehen
+        <br />
+        Ein letzes Lied, ein letzter Kuss
+        <br />
+        Kein Wunder wird geschehen
+        <br />
+        Adieu, Goodbye, Auf Wiedersehen
+        <br />
+        Die Zeit mit dir war schön
+        <br />
+        Aus dem Leben steigst du leise
+        <br />
+        Die Seele zieht auf stille Reise
+        <br />
+        Fleisch vergeht, Geist wird sich heben
+        <br />
+        Das sein wird sich dem Tod ergeben
+        <br />
+        Ja
+        <br />
+        Am Ende bist du ganz allein
+        <br />
+        Doch wir werden bei dir sein
+        <br />
+        Adieu, Goodbye, Auf Wiedersehen
+        <br />
+        Den letzten Weg musst du alleine gehen
+        <br />
+        Ein letzes Lied, ein letzter Kuss
+        <br />
+        Kein Wunder wird geschehen
+        <br />
+        Adieu, Goodbye, Auf Wiedersehen
+        <br />
+        Die Zeit mit dir war schön
+        <br />
+        Jeder stirbt für sich allein
+        <br />
+        Ganz allein
+        <br />
+        Doch du wirst immer bei uns sein
+        <br />
+        Adieu, Goodbye, Auf Wiedersehen
+        <br />
+        Den letzten Weg musst du alleine gehen
+        <br />
+        Ein letzes Lied, ein letzter Kuss
+        <br />
+        Kein Wunder wird geschehen
+        <br />
+        Adieu, Goodbye, Auf Wiedersehen
+        <br />
+        Die Zeit mit dir war schön
+        <br />
+      </p>
+      <h1>Goodbye </h1>
     </div>
   );
 }
